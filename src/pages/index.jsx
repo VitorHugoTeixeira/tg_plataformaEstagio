86c07722942942fd81f99c72ab61154d6ff51c6d
Vitor Hugo <vitor4oliveira@gmail.com>
2023-05-07 15:47:12 -0300

    Adicionando Painel da Faculdade

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -14,16 +14,17 @@ import Duvidas from '../components/Duvidas'
 import PainelAluno from '../components/PainelAluno'
 import ScrollableTabsButtonAuto from '../components/Teste'
 import PainelOrientador from '../components/PainelOrientador'
+import PainelCoordenacao from '../components/PainelCoordenacao'
 
 export default function Home() {
   return (
     <>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@100;200;300;400;500;600;700;800;900&family=Source+Serif+Pro:wght@200;300;400;600;700;900&display=swap" rel="stylesheet" />
       </Head>
-      <PainelOrientador/>
+      <PainelCoordenacao />
     </>
   )
 }
